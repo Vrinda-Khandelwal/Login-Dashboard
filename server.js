8d469edc6cb85ab0cfd521d2e0367a836281e367
Vrinda-Khandelwal <vrindakhandelwal2002@gmail.com>
2025-07-19 21:22:04 +0530

    change in fetch links

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ server.use(router);
 // Use the port assigned by Render (or fallback to 3001 for local testing)
 const PORT = process.env.PORT || 3001;
 
+//const PORT = process.env.PORT || 10000;
+
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Visit this in production: https://securelytix-api.onrender.com`);
 });
